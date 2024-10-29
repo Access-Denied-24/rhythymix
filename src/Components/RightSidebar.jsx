915999360d324b1bb81c8343731bfd6c73a323cb
Anish Sayana <anishsayana@gmail.com>
2024-10-29 22:29:51 +0530

    Added Sidebars

---
+++ src/Components/RightSidebar.jsx
@@ -0,0 +1,63 @@
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+export default function RightSidebar(){
+  return (
+    <>
+  <div className="absolute right-0 top-[90px] bottom-0 flex flex-col bg-clip-border rounded-xl bg-neutral-800 text-white w-60 p-4 shadow-xl shadow-blue-gray-900/5">
+    <div className="mb-2 p-4">
+      <h5 className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-white">
+        Active Songs
+      </h5>
+    </div>
+    <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-white">
+
+      <div
+        role="button"
+        tabIndex={0}
+        className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+      >
+        <div className="grid place-items-center mr-4">
+          {/* <AccountCircleIcon /> */}
+        </div>
+        Lyrics
+      </div>
+      <div
+        role="button"
+        tabIndex={0}
+        className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+      >
+        <div className="grid place-items-center mr-4">
+          {/* <SettingsIcon /> */}
+        </div>
+        Song Details
+      </div>
+      
+      <div
+        role="button"
+        tabIndex={0}
+        className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+      >
+        <div className="grid place-items-center mr-4">
+          {/* <SettingsIcon /> */}
+        </div>
+        Artist Details
+      </div>
+
+      <div // logout
+        role="button"
+        tabIndex={0}
+        className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+      >
+        <div className="grid place-items-center mr-4">
+          {/* <PowerSettingsNewIcon /> */}
+        </div>
+        Next in Queue
+      </div>
+    </nav>
+  </div>
+   
+    </>
+  );
+}
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,9 +1,13 @@
 import Navbar from "../Components/Navbar";
+import LeftSidebar from "../Components/LeftSidebar";
+import RightSidebar from "../Components/RightSidebar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <LeftSidebar />
+      <RightSidebar />
     </>
   );
 }
