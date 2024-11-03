2b9797d39b03e9d612d587aeb8f52ea8cc9f1d2e
Ashish Singh <ashishsingh.baradhi@gmail.com>
2024-11-03 21:05:09 +0530

    fix some error

--- src/App.jsx
+++ src/App.jsx
@@ -33,19 +33,9 @@ const router = createBrowserRouter([
     element: <ProfilePage />
   },
   {
-<<<<<<< HEAD
     path: "/resetpass/:token",
     element: <respas />
   },
-=======
-    path: "/tracks",
-    element: <TracksPage />
-  },
-  // {
-  //   path: "/resetpass",
-  //   element: <respas />
-  // },
->>>>>>> 6cb34e50171e8ed6d4ea4ba0177770b6fb0219bf
   {
 
   }
